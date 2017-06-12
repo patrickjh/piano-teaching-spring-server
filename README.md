72e1a4b4dccbc746eada5d3adc38ce4f6d7d3f5b
patrickjh <patrick@lessonswithpatrick.com>
2017-06-12 01:02:50 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# piano-teaching-spring-server" 
---
+++ src/main/java/greeting/Application.java
@@ -0,0 +1,16 @@
+package greeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by patrick on 6/12/2017.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
